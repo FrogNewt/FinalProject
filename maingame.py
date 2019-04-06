9482eed9860fc42ddee9e7b1ac36f61688661e03
Jake Botello <botellj@gmail.com>
2019-04-06 13:52:20 -0500

    added a basic function for getting activities done that day

--- maingame.py
+++ maingame.py
@@ -3,6 +3,32 @@
 import sys
 import re
 
+# Saves a greeting for use later
 greeting = print("WELCOME TO--*ahem*--welcome to the game. What did you do, today?")
 
-usractivity = input("")
+# Gets whatever the user did that day as input.
+
+
+#Generates an empty list to be populated by the user's activities
+activitydict = {"walk" : 5}
+
+expdict = {"exercise" : 0, "academics" : 0, "social": 0, "professional" : 0}
+
+def getactivities():
+	activity = input("")
+	if activity.lower() in activitydict.keys():
+		print("You've done that one before!")
+		print("It's worth {0} exp!".format(activitydict[activity]))
+	else:
+		print("That's a new one--how many experience points is it worth?  (Give a number between 1 and 50!)")
+		activityexp = input("")
+		activitydict[activity.lower()] = activityexp
+		for category in expdict.keys():
+			print(category.title())
+		print("To which category should I assign that exp?  You can assign it to any of the above categories:\n")
+		catchoice = input("")
+
+getactivities()
+
+print(expdict)
+print(activitydict)
